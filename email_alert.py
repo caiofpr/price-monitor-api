2c3aca2dfb0eca2f5b3b292523c331fbaba1f102
Caio <caiofpetraglia@gmail.com>
2026-04-17 00:17:26 -0300

    switch to gmail smtp

--- email_alert.py
+++ email_alert.py
@@ -1,16 +1,21 @@
-import resend
-import os
+import smtplib
+from email.mime.text import MIMEText
 
-resend.api_key = os.getenv("RESEND_API_KEY") 
+EMAIL_REMETENTE = "monitorapiemail@gmail.com"
+SENHA_APP = "pndh ooop frkf mabt"
 
 def enviar_email(destino, assunto, mensagem):
     try:
-        resend.Emails.send({
-            "from": "onboarding@resend.dev",
-            "to": [destino],
-            "subject": assunto,
-            "text": mensagem,
-        })
+        msg = MIMEText(mensagem)
+        msg["Subject"] = assunto
+        msg["From"] = EMAIL_REMETENTE
+        msg["To"] = destino
+
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(EMAIL_REMETENTE, SENHA_APP)
+            server.send_message(msg)
+
         print(f"Email enviado para {destino}")
 
     except Exception as e:
